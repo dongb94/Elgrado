0afe5fef2115877d07f73862eee8b0799a07922a
DONGGEON BYEON <dongb94@gmail.com>
2018-09-05 15:25:59 +0900

    Add OutlineMaker

---
+++ OutlineMaker.cs
@@ -0,0 +1,66 @@
+﻿// #define VISUAL_DEBUG
+
+using UnityEngine;
+
+public class OutlineMaker : MonoBehaviour
+{
+	#region <Consts>
+
+	// min distance offset
+	private const float DistanceToCalculateOutline = 70f;
+	// property
+	private const string PropertyName = "_OutlineWidth";
+	
+	#endregion </Consts>
+
+	#region <Fields>
+
+	// body, weapon, shield
+	private RaycastHit _raycastHit;
+	private Transform _transform;
+	private Unit _mUnit;
+	private K514MaterialApplier _mMaterialApplier;
+
+    private Champion _champion
+    {
+        get { return PlayerManager.GetInstance.PlayerChampion; }
+    }
+
+    #endregion </Fields>
+
+    #region <Unity/Callbacks>
+
+    private void Awake()
+	{
+        // @SEE: head is the best place, maybe
+        _raycastHit = new RaycastHit();
+        _transform = GetComponent<Transform>();
+		_mUnit = GetComponent<Unit>();
+		_mMaterialApplier = GetComponent<K514MaterialApplier>();
+	}
+	
+	private void Update()
+	{
+		if (Vector3.SqrMagnitude(_champion.transform.position-_transform.position) < DistanceToCalculateOutline && _mUnit.State != Unit.UnitState.Dead)
+		{
+			CheckOutline();
+		}
+	}
+	
+	#endregion
+
+	#region <Methods>
+
+	private void CheckOutline()
+	{
+        var camera_position = CameraManager.GetInstance.MainCameraTransform.position;
+        Ray ray = new Ray(_transform.position + new Vector3(0, 2f, 0), camera_position - _transform.position - new Vector3(0, 2f, 0));
+        bool hit = Physics.Raycast(ray,out _raycastHit, Mathf.Infinity);
+        if (hit)
+            _mMaterialApplier.SetTrigger(K514MaterialStorage.MAT_STATE.KOutLine);
+        else
+            _mMaterialApplier.RevertTrigger();
+
+    }
+	#endregion
+}
